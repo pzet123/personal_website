c9fc59167421e1aa996cdcaaef74da469566ac43
pzet <soniapiotr@gmail.com>
2022-03-30 17:40:04 +0100

    Changed style, fixed up layout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+$(function(){
+
+    if($(window).width() > 768){
+        $("#navButton").hide();
+    }
+
+    $(window).resize(function() {
+        if($(window).width() > 768){
+            $("#toggleMenu").show()
+            $("#navButton").hide()
+        }
+        if($(window).width() <= 768){
+            $("#toggleMenu").hide()
+            $("#navButton").show()
+        }
+
+    })
+});
+
+
 
 function toggleMenu(){
     $("#toggleMenu").toggle("height");
