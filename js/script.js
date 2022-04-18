70421b6594cdc0c5a53a19282177ba922279b49f
pzet <soniapiotr@gmail.com>
2022-04-18 22:49:00 +0100

    Finished projects and added personal info

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(function(){
-
     if($(window).width() > 768){
         $("#navButton").hide();
     }
@@ -22,3 +21,27 @@ $(function(){
 function toggleMenu(){
     $("#toggleMenu").toggle("height");
 }
+
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+    SlideShow(slidePosition += n);
+}
+
+//  images controls
+function currentSlide(n) {
+    SlideShow(slidePosition = n);
+}
+
+function SlideShow(n) {
+var i;
+var slides = document.getElementsByClassName("project2Image");
+if (n > slides.length) {slidePosition = 1}
+if (n < 1) {slidePosition = slides.length}
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+}
+slides[slidePosition-1].style.display = "block";
+} 
